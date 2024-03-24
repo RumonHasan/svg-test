282904b89407a89541c4a91bec2f4c34d9e033a3
rumon hasan <rumon@rumonnonotobukkukonpyuta.local>
2024-03-24 17:14:30 +0900

    map update

--- src/components/SVGApproach/SVGApproach.jsx
+++ src/components/SVGApproach/SVGApproach.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect, useRef, memo } from 'react';
-import JapanMap from '../../assets/japan.svg';
+import jpSvg from '../../assets/jp_2.svg';
+
 import { ReactSVG } from 'react-svg';
 import './styles.css';
 // images for pref
@@ -36,7 +37,7 @@ const CustomToolTip = memo(({ pref_label, x_pos, y_pos }) => {
 
 // cusom svg approach
 const SVGApproach = () => {
-  const [map, setMap] = useState(JapanMap);
+  const [map, setMap] = useState(jpSvg);
   const mapRef = useRef();
   // data to update tooltip data
   const [tooltipData, setToolTipData] = useState({
@@ -47,8 +48,8 @@ const SVGApproach = () => {
 
   // setting the svg data to map
   useEffect(() => {
-    if (JapanMap) {
-      setMap(JapanMap);
+    if (jpSvg) {
+      setMap(jpSvg);
     }
   }, []);
 
@@ -60,34 +61,17 @@ const SVGApproach = () => {
     const path_map = new Map();
     for (let pathIndex = 0; pathIndex < paths.length; pathIndex++) {
       const current_path = paths[pathIndex];
-      path_collection.push(current_path);
       const pref_title = current_path.getAttribute('title');
-      const pref_id = current_path.id;
-      path_map.set(pref_id, pref_title);
+      if (pref_title !== '') {
+        const pref_id = current_path.id;
+        path_map.set(pref_id, pref_title);
+        path_collection.push(current_path);
+      }
     }
-
-    // modifications to prefs
+    // modify each path
     path_collection.forEach((path) => {
-      path.style.cursor = 'pointer';
-      // adding on hover
-      path.addEventListener('mouseover', (path_event) => {
-        const path_x_position = path_event.clientX;
-        const path_y_position = path_event.clientY;
-        const pref_label = path_map.get(path.id);
-        setToolTipData({
-          prefLabel: pref_label,
-          x_pos: path_x_position,
-          y_pos: path_y_position,
-        });
-      });
-
-      // removing it
-      path.addEventListener('mouseout', () => {
-        setToolTipData({
-          prefLabel: '',
-          x_pos: 0,
-          y_pos: 0,
-        });
+      path.addEventListener('mouseover', () => {
+        path.style.fill = 'red';
       });
     });
   };
@@ -106,18 +90,18 @@ const SVGApproach = () => {
           src={map}
           useRequestCache={false}
           fallback={() => <span>Error!</span>}
-          style={{ transform: 'scale(1.5)', transformOrigin: 'top left' }}
+          // style={{ transform: 'scale(0.5)', transformOrigin: 'top left' }}
         />
       </div>
 
       <div>
-        {tooltipData.prefLabel !== '' && (
+        {/* {tooltipData.prefLabel !== '' && (
           <CustomToolTip
             pref_label={tooltipData.prefLabel}
             x_pos={tooltipData.x_pos}
             y_pos={tooltipData.y_pos}
           />
-        )}
+        )} */}
       </div>
     </div>
   );
